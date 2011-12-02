c9927c156bf0cd64247cae00fb440c5553b797a1
donnfelker <donn@donnfelker.com>
2011-12-02 15:04:30 -0700

    Adding README and button click to show that bubble clicks pass through to button

--- src/com/example/MainActivity.java
+++ src/com/example/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 import roboguice.activity.RoboActivity;
 import roboguice.inject.InjectView;
 
@@ -9,7 +11,8 @@ public class MainActivity extends RoboActivity
 {
 
     @InjectView(R.id.exampleText) protected TextView exampleText;
-
+    @InjectView(R.id.bubble_button) protected TextView bubbleButton;
+    
     @Override
     public void onCreate(Bundle savedInstanceState)
     {
@@ -17,6 +20,16 @@ public class MainActivity extends RoboActivity
         setContentView(R.layout.main);
 
         exampleText.setText( R.string.friend );
+        
+
+        
+        bubbleButton.setOnClickListener( new View.OnClickListener() {
+            int pressedTimes = 0;
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Button was pressed! (" + (pressedTimes++) + ")", Toast.LENGTH_SHORT).show();
+            }
+        });
 
     }
 }
